9341c19fa1f807ca2b2da26813de1d44986def53
Utkarsh-Rai22 <22utkarshrai@gmail.com>
2023-07-18 07:52:45 +0530

    18/07/2023

---
+++ Section Four/Chorus.java
@@ -0,0 +1,15 @@
+class Chorus{
+    public static void main(String[] args) {
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+    } 
+    public static void singChorus(){
+        System.out.println("Dont 1");
+        System.out.println("Dont 2");
+        System.out.println("Dont 3");
+        System.out.println("Dont 4");
+    }
+}

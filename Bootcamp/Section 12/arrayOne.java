f3f5fb5001ec8dda7d6e6d9960f17b088fd1e40b
Utkarsh-Rai22 <22utkarshrai@gmail.com>
2023-07-30 08:59:51 +0530

    Workbook 6.1 to 6.4

---
+++ Bootcamp/Section 12/arrayOne.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import javax.print.DocFlavor.STRING;
+
+public class arrayOne{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //workbook 6.1 & 6.2
+        // String [] kingdom={"Harry", "Nevilee", "Ron", "Hermione", "Seamus"};
+        // for(int i =0; i<kingdom.length; i++){
+        //     System.out.println(kingdom[i]+" will take seat "+i);
+        // }
+        //workbook 6.3 & 6.4
+        // System.out.println("Enter your choice");
+        // String choice= sc.nextLine();
+        // String [] aisle={"", "Apples", "Bananas", "Candy", "Chocolate", "Coffee", "Tea"};
+        // for(int i=0; i < aisle.length; i++){
+            // if(aisle[i].equals(choice)){
+            // System.out.println("We have the item in aisle: " + i);
+            // }
+            // if(i==aisle.length){
+                // System.out.println("We don't have it!");
+                // break;
+            // } 
+        // }
+        // workbook 6.5
+        
+    }
+}

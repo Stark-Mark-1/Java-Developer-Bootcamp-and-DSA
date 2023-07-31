add9fafc71d6eab598c56bf6e81667e3ce13675b
Utkarsh-Rai22 <22utkarshrai@gmail.com>
2023-07-31 22:48:52 +0530

    Section 3 Arrays Complete!

--- Array2d.java
+++ Array2d.java
@@ -4,8 +4,8 @@ public class Array2d{
     int arr[][]=null;
     public Array2d(int numOfRow, int numOfCol){
         this.arr =new int[numOfRow][numOfCol];
-        for(int r=0; r< arr.length; i++){
-            for(int c=0; c<arr.length; c++){
+        for(int r=0; r< numOfRow; r++){
+            for(int c=0; c<numOfCol; c++){
                 arr[r][c]= Integer.MIN_VALUE;
             }
         }
@@ -23,7 +23,7 @@ public class Array2d{
             System.out.println("Invalid Index value");
         }
     }
-    public void accessTheArray(int row, int col){
+    public void accessTheArray(int row, int col){ // function to access the value of a cell in 2d array
         try{
             if(arr[row][col]==Integer.MIN_VALUE){
                 System.out.println("The cell is empty");
@@ -34,7 +34,33 @@ public class Array2d{
             System.out.println("Invalid Index");
         }
     }
-    
+    public void traverseTheArray(){
+        for(int row=0; row < arr.length; row ++){
+            for(int col =0 ; col<arr.length;col++){
+                System.out.println(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public void findElementInTheArray(int val){
+        for(int r=0; r< arr.length; r++){
+            for(int c=0; c<arr.length; c++){
+                if(arr[r][c]== val){
+                    System.out.println("The elemement is present at row: " + r +" c: " + c);
+                }else{
+                    System.out.println("The element is not present in Array");
+                }
+            }
+        }
+    }
+    public void deleteElementFromArray(int row, int col){
+        try{
+            arr[row][col]=Integer.MIN_VALUE;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Invvalid Cell");
+        }
+    }
+
    // public static void main(String[] args) {
     //     Scanner sc = new Scanner(System.in);
     //     int [][] int2dArray = new int[2][2];
--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 import java.util.*;
-import java.util.Arrays;
+
+
 public class Main {
-    Array2d sda= new Array2d(3, 3);
-    sda.insertValueInTheArray(0,0, 10);
+    public static void main(String[] args) {
+        Array2d objArray2d = new Array2d(3, 3);
+        objArray2d.insertValueInTheArray(0,0,10);
+
+
+    }
+    // int arr[][]=new int[2][3];
 }
